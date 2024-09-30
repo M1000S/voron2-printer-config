8084e2c78b51533222e4bbad1a5fe249cfc78c95
M1000S <peter.sarkar@web.de>
2024-09-30 21:01:23 +0200

    Autocommit from 2024-09-30 21:01:23
    Klipper on commit: f0753bd
    
    Moonraker on commit: 6d8cb76
    
    Mainsail version: v2.8.0

--- scripts/airfilter/airfilter_ctrl.sh
+++ scripts/airfilter/airfilter_ctrl.sh
@@ -4,13 +4,13 @@ cmd=$1
 
 if [ $cmd = "power_on" ]; then
 	#echo "power_on"
-    wget -O /dev/null -q 'http://shelly1pm-ruckfan/relay/0?turn=on'
+    wget -O /dev/null -q 'http://shelly1pm-ruckfan.home.sarkar.net/relay/0?turn=on'
 elif [ $cmd = "power_off" ]; then
 	#echo "power_off"
-    wget -O /dev/null -q 'http://shelly1pm-ruckfan/relay/0?turn=off'
+    wget -O /dev/null -q 'http://shelly1pm-ruckfan.home.sarkar.net/relay/0?turn=off'
 elif [ $cmd = "keep_alive" ]; then
 	#echo "keep_alive"
-    wget -O /dev/null -q 'http://tasmota-ruckfan/cm?cmnd=script>FanReqOn=1'
+    wget -O /dev/null -q 'http://tasmota-ruckfan.home.sarkar.net/cm?cmnd=script>FanReqOn=1'
 else
 	echo "invalid command"
 fi
