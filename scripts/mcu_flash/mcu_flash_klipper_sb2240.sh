93153bba9b04bcc007627bc2fff670e400476931
M1000S <peter.sarkar@web.de>
2024-10-24 12:37:18 +0200

    Autocommit from 2024-10-24 12:37:18
    Klipper on commit: 96cceed
    
    Moonraker on commit: 71f9e67
    
    Mainsail version: v2.12.0

--- scripts/mcu_flash/mcu_flash_klipper_sb2240.sh
+++ scripts/mcu_flash/mcu_flash_klipper_sb2240.sh
@@ -9,7 +9,9 @@ mv ~/printer_data/config/scripts/mcu_flash/config/klipper/config.make.klipper.sb
 make clean KCONFIG_CONFIG=config.make.klipper.sb2240
 make menuconfig KCONFIG_CONFIG=config.make.klipper.sb2240
 make KCONFIG_CONFIG=config.make.klipper.sb2240
-python3 ~/CanBoot/scripts/flashtool.py -i can0 -f ~/klipper/out/klipper.bin -u 79a866722924 
+#python3 ~/CanBoot/scripts/flashtool.py -i can0 -f ~/klipper/out/klipper.bin -u 79a866722924
+#python3 ~/CanBoot/scripts/flashtool.py -i can0 -f ~/klipper/out/klipper.bin -u c19d9edaf4c0
+python3 ~/CanBoot/scripts/flashtool.py -i can0 -f ~/klipper/out/klipper.bin -u 6c25b7e5f737
 mv ~/klipper/config.make.klipper.sb2240 ~/printer_data/config/scripts/mcu_flash/config/klipper/
 
 sudo systemctl start klipper
